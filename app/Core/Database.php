4a46d4c1799d85a5118312694f8c13da3ba73a42
hamza <hamzaprogeame@gmail.com>
2025-02-08 18:50:41 +0100

    setting up the project structure

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -1,8 +1,41 @@
 <?php
+    
+    namespace App\Core;
 
-namespace App\Core;
+    use PDO;
+    use PDOException;
 
-class Database
-{
-
-}
+    class Database {
+        private static $conn;
+        private static $instance = null;
+    
+        private $host = 'host.docker.internal';  
+        private $db = 'Eventbit_DB';  
+        private $user = 'user';  
+        private $pass = 'root_password';  
+        private $port = 5432;
+    
+        private function __construct() {
+            try {
+                $dsn = "pgsql:host=$this->host;dbname=$this->db;port=$this->port";
+                self::$conn = new PDO($dsn, $this->user, $this->pass);  
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+               
+            } catch (PDOException $e) {
+                die("Database connection failed: " . $e->getMessage());
+            }
+        }
+    
+        public static function getInstance() {
+            if (self::$instance === null) {
+                self::$instance = new self();
+            }
+            return self::$instance;
+        }
+    
+        public static function getConnection() {
+            return self::$conn;
+        }
+    
+ 
+    }
--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
-<?php
+<?php 
+
+
+require __DIR__. "/../vendor/autoload.php";
+
+use App\Core\Database;
+
+Database::getInstance();
+$stmt = Database::getConnection();
+$data = $stmt->prepare("SELECT * FROM users");
+$data->execute();
+$users = $data->fetchAll(\PDO::FETCH_ASSOC);
+
+print_r($users);
